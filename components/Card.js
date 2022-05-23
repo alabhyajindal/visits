23f877005af9b79ed2f2b391f29531e3f2a2ee9a
Alabhya Jindal <alabhya10@gmail.com>
2022-05-23 14:51:59 +0530

    Styled the Card component

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,16 @@
 import Image from 'next/dist/client/image';
 
-export default function Card({ image, title }) {
+export default function Card({
+  image,
+  title,
+  location,
+  maxGuests,
+  price,
+  duration,
+}) {
   return (
-    <div>
-      <div className='bg-gray-400 relative aspect-w-16 aspect-h-9'>
+    <div className='text-gray-700 rounded-sm hover:bg-gray-100 cursor-pointer transition transform duration-200 ease-out p-2'>
+      <div className='aspect-h-9 aspect-w-16 relative'>
         <Image
           src={image}
           layout='fill'
@@ -11,8 +18,17 @@ export default function Card({ image, title }) {
           className='rounded-sm'
         />
       </div>
-      <div>
-        <h1>{title}</h1>
+      <div className='p-2'>
+        <h1 className='text-xl font-medium '>
+          {title},{' '}
+          <span className='font-normal text-gray-600 text-lg'>{location}</span>
+        </h1>
+        <div className='text-gray-500'>
+          <p className='text-lg'>
+            ₹{price} for {maxGuests} visitors
+          </p>
+          <p className='text-gray-350'>{duration} hours guided tour</p>
+        </div>
       </div>
     </div>
   );
