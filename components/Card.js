d08bcd1a5651c0519774a67fc1dadd2424502a00
Alabhya Jindal <alabhya10@gmail.com>
2022-05-26 17:30:44 +0530

    Replaced anchor tag with next/link to benefit prefetching

--- components/Card.js
+++ components/Card.js
@@ -11,35 +11,37 @@ export default function Card({
   duration,
 }) {
   return (
-    <a href={`/visits/${id}`} target='_blank' rel='noreferrer'>
-      <div className='text-gray-700 rounded-md hover:bg-gray-100 cursor-pointer transition transform duration-200 ease-out p-2'>
-        {image && (
-          <div className='aspect-h-9 aspect-w-16 relative'>
-            <Image
-              src={image}
-              layout='fill'
-              objectFit='cover'
-              className='rounded-md'
-              alt={image.title}
-            />
-          </div>
-        )}
-        <div className='p-2'>
-          <h1 className='text-xl font-medium text-gray-700'>
-            {title},{' '}
-            <span className='font-normal text-gray-600 text-lg'>
-              {location}
-            </span>
-          </h1>
-          <div className='text-gray-500'>
-            <p className='text-lg'>
-              ₹{Intl.NumberFormat('en-IN').format(price)} for {maxGuests}{' '}
-              visitors
-            </p>
-            <p className='text-gray-350'>{duration} hours guided tour</p>
+    <Link href={`/visits/${id}`}>
+      <a target='_blank'>
+        <div className='text-gray-700 rounded-md hover:bg-gray-100 cursor-pointer transition transform duration-200 ease-out p-2'>
+          {image && (
+            <div className='aspect-h-9 aspect-w-16 relative'>
+              <Image
+                src={image}
+                layout='fill'
+                objectFit='cover'
+                className='rounded-md'
+                alt={image.title}
+              />
+            </div>
+          )}
+          <div className='p-2'>
+            <h1 className='text-xl font-medium text-gray-700'>
+              {title},{' '}
+              <span className='font-normal text-gray-600 text-lg'>
+                {location}
+              </span>
+            </h1>
+            <div className='text-gray-500'>
+              <p className='text-lg'>
+                ₹{Intl.NumberFormat('en-IN').format(price)} for {maxGuests}{' '}
+                visitors
+              </p>
+              <p className='text-gray-350'>{duration} hours guided tour</p>
+            </div>
           </div>
         </div>
-      </div>
-    </a>
+      </a>
+    </Link>
   );
 }
