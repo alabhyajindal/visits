3e4b899f21077c7b3be5f5546142c3389057f81f
Alabhya Jindal <alabhya10@gmail.com>
2022-05-31 18:15:10 +0530

    Reverted back to using system font stack Added notifications post checkout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import Banner from '../components/Banner';
 
 export default function Visit() {
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get('success')) {
+      toast.success('Visit booked! You will receive an email confirmation.');
+    }
+
+    if (query.get('canceled')) {
+      toast.error(
+        `Order canceled, continue to explore and book a visit when you're ready.`
+      );
+    }
+  }, []);
+
   return (
     <div>
       <Head>
--- pages/visits/[id].js
+++ pages/visits/[id].js
@@ -11,20 +11,6 @@ const stripePromise = loadStripe(
 );
 
 export default function ListedVisit(visit) {
-  useEffect(() => {
-    // Check to see if this is a redirect back from Checkout
-    const query = new URLSearchParams(window.location.search);
-    if (query.get('success')) {
-      console.log('Order placed! You will receive an email confirmation.');
-    }
-
-    if (query.get('canceled')) {
-      console.log(
-        'Order canceled -- continue to shop around and checkout when you’re ready.'
-      );
-    }
-  }, []);
-
   return (
     <div>
       <Head>
@@ -66,6 +52,7 @@ export default function ListedVisit(visit) {
           Book
         </button>
       </form>
+      <Toaster />
     </div>
   );
 }
