51fc57cd41fcafdfdb5550c7158fd36cbb0682f4
Alabhya Jindal <alabhya10@gmail.com>
2022-06-01 16:17:18 +0530

    Changed gradient

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <div className='overflow-x-hidden rounded-md mb-16'>
-      <div className='relative w-screen h-[32rem] bg-gradient-to-br from-indigo-800 via-green-500 to-blue-500'>
+      <div className='relative w-screen h-[32rem] bg-gradient-to-br from-purple-800 via-indigo-700 to-blue-500'>
         <h1 className='absolute left-1/2 -translate-x-2/3 translate-y-3/4 lg:translate-y-1/2 text-5xl lg:text-7xl w-2/3 max-w-md font-semibold text-white'>
           Streamlined industrial visits for everyone
         </h1>
